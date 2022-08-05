23acd6ad569a575209ba60f174a3ea061a7f8ede
jayendra_madara <dimebeatengreen8@gmail.com>
2022-08-05 15:15:53 +0530

    Comments for team members to get along

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,16 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Signup from "./Components/Signup";
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Routes, Route } from "react-router-dom";
+// App made with vite
 
 function App() {
+  /*
+    React router used for routing 
+    default 404 
+    current routes = ["/" , "/Signup"]
+   */
   return (
     <div>
       <Routes>
@@ -13,13 +20,20 @@ function App() {
           path='/'
           element={
             <div>
-              <div className='flex justify-center items-center h-screen text-2xl font-bold text-slate-500'>
-                HI MOM
+              <div className='flex flex-col justify-center items-center h-screen text-2xl font-bold text-slate-500'>
+                <div>HI MOM</div>
+
+                <a href='/Signup' className='underline'>
+                  SIGNUP
+                </a>
               </div>
             </div>
           }
         ></Route>
+
         <Route path='/Signup' element={<Signup />}></Route>
+
+        {/* default Route */}
         <Route path='*' element={<div>404</div>} />
       </Routes>
     </div>
