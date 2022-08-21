4b26139765cde3e09be3b54c43ab292485fff6b0
Sahil Apte <sahilapte14@gmail.com>
2022-08-21 18:46:13 +0530

    state page changes

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,8 @@ import GPI from "./Pages/GPI";
 import DropOut from "./Pages/DropOut";
 import LiteracyRate from "./Pages/Literacy";
 import Standard from "./Pages/Standard";
+import State from "./Pages/State" ;
+import { States } from "./constants";
 import Testin from "./Pages/testing";
 function App() {
   /*
@@ -42,6 +44,7 @@ function App() {
         {/* <Route path='/test' element={<Testin />}></Route> */}
         {/* default Route */}
         <Route path='/levels/:level' element={<Standard />}></Route>
+        <Route path='/State/:states' element={<State />}></Route>
         <Route path='*' element={<div>404</div>} />
       </Routes>
     </div>
