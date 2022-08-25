b713576d38fbc60d9304b89262f3ec6459260406
jayendra_madara <dimebeatengreen8@gmail.com>
2022-08-26 01:44:20 +0530

    vijju ki changes

--- src/Pages/testing.tsx
+++ src/Pages/testing.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { States } from "../constants"
+import { States, StatesXcolor } from "../constants"
 
 function Testin(props: { val: string }) {
     const filter = props.val.toUpperCase()
@@ -8,11 +8,11 @@ function Testin(props: { val: string }) {
         <div className='w-full py-4  bg-bgr2 rounded-lg shadow-lg shadow-slate-400'>
             <ul className='w-full '>
                 {
-                    States.map((elem, index) =>
-                        <li key={index} className={`w-full px-5 border-b py-3 text-slate-300 text-md font-mono ${elem.toUpperCase().indexOf(filter) > -1 ? "" : "hidden"}`}>
-                            <Link to={`/states/${elem.replace(" ", "")}`} onClick={() => console.log(elem)
+                    StatesXcolor.map((elem, index) =>
+                        <li key={index} className={`w-full px-5 border-b py-3 text-slate-300 text-md font-mono ${elem.name.toUpperCase().indexOf(filter) > -1 ? "" : "hidden"}`}>
+                            <Link to={`/state/${elem.State.replace(" ", "")}`} onClick={() => console.log(elem)
                             }>
-                                {elem}
+                                {elem.name}
                             </Link>
                         </li>
                     )
